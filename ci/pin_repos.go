5c5708ab30c541f472fba2297094c5bf697d5eab
Mitch Kelley <mitch.kelley@solo.io>
2019-04-10 07:27:42 -0400

    pin repos with fetch

--- ci/pin_repos.go
+++ ci/pin_repos.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+
+	"github.com/solo-io/go-utils/errors"
 	version "github.com/solo-io/go-utils/versionutils"
 	"github.com/solo-io/solo-kit/pkg/utils/log"
 )
@@ -17,6 +24,9 @@ func main() {
 
 	fatalCheck(version.PinGitVersion("../solo-kit", soloKitVersion), "consider git fetching in solo-kit repo")
 
+	if err := FetchGitRepo("../gloo"); err != nil {
+		fmt.Printf("Error while fetching gloo: %v\n", err)
+	}
 	fatalCheck(version.PinGitVersion("../gloo", glooVersion), "consider git fetching in gloo repo")
 }
 
@@ -24,4 +34,18 @@ func fatalCheck(err error, msg string) {
 	if err != nil {
 		log.Fatalf("Error (%v) unable to pin repos!: %v", msg, err)
 	}
+}
+
+func FetchGitRepo(relativeRepoDir string) error {
+	tag := GetTag(version)
+	cmd := exec.Command("git", "fetch", origin)
+	cmd.Dir = relativeRepoDir
+	buf := &bytes.Buffer{}
+	out := io.MultiWriter(buf, os.Stdout)
+	cmd.Stdout = out
+	cmd.Stderr = out
+	if err := cmd.Run(); err != nil {
+		return errors.Wrapf(err, "%v failed: %s", cmd.Args, buf.String())
+	}
+	return nil
 }
