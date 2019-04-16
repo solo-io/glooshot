dc35fa6aa53ecf683c856689a10bccc8b7f7625f
Mitch Kelley <mitch.kelley@solo.io>
2019-04-16 14:09:37 -0400

    flatten makefile, phases -> bolean-release

--- make/update_deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-go get -u golang.org/x/tools/cmd/goimports
-go get -u github.com/gogo/protobuf/gogoproto
-go get -u github.com/gogo/protobuf/protoc-gen-gogo
-go get -u github.com/envoyproxy/protoc-gen-validate
-go get -u github.com/paulvollmer/2gobytes
