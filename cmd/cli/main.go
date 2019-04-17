f0342c3c5a9f4c3cdb2926cb1ba5d1c722ca18bc
Mitch Kelley <mitch.kelley@solo.io>
2019-04-17 16:05:35 -0400

    use os.Create for log file

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
-
+	"github.com/mitchellh/go-homedir"
 	"go.uber.org/zap/zapcore"
+	"os"
+	"path/filepath"
 
 	"github.com/solo-io/glooshot/pkg/cli"
 	"github.com/solo-io/glooshot/pkg/version"
@@ -14,21 +15,59 @@ import (
 	"github.com/solo-io/go-utils/contextutils"
 )
 
-// TODO - provide this documentation
-func GetCliLoggerOutputModeDescriptions(key string) string {
-	return fmt.Sprintf(`output modes:
-1: verbose - all content logged to console
-specify mode with: %s
-`, key)
+var outputModeEnvVar = "GLOOSHOT_CLI_OUTPUT_MODE"
+
+func getInitialContextAndSetLogger() context.Context {
+	contextutils.SetFallbackLogger(BuildCliLogger([]string{".glooshot", "log"}, outputModeEnvVar))
+	loggingContext := []interface{}{"version", version.Version}
+	ctx := contextutils.WithLogger(context.Background(), version.CliAppName)
+	ctx = contextutils.WithLoggerValues(ctx, loggingContext...)
+	return ctx
 }
 
-func buildCliZapCoreFile(filename string, verboseMode bool) zapcore.Core {
-	file, err := os.Open(filename)
+func main() {
+	ctx := getInitialContextAndSetLogger()
+	app := cli.App(ctx, version.Version)
+	if err := app.Execute(); err != nil {
+		contextutils.LoggerFrom(ctx).Fatalw("error during glooshot cli execution", zap.Error(err))
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// TODO - move this to go-utils/contextutils/context_and_logging.go
+////////////////////////////////////////////////////////////////////////////////
+func FilePathFromHomeDir(pathElementsRelativeToHome []string) (string, error) {
+	// Find home directory.
+	home, err := homedir.Dir()
+	if err != nil {
+		return "", err
+	}
+	pathElements := append([]string{home}, pathElementsRelativeToHome...)
+	return filepath.Join(pathElements...), nil
+}
+
+func buildCliZapCoreFile(pathElements []string, verboseMode bool) zapcore.Core {
+	path, err := FilePathFromHomeDir(pathElements)
 	if err != nil {
 		if verboseMode {
 			// we don't want to return errors just because we cannot write logs to a file
 			// users can use the verbose flag to get full output to the console
-			fmt.Printf("Could not open log file %s for writing: %v\n", filename, err)
+			fmt.Printf("Could not open log file %s for writing: %v\n", filepath.Join(pathElements...), err)
+		}
+		return nil
+	}
+
+	// if we decide we want to append logs, we can do it this way:
+	// (we would be required to first create the file and "rotate" it as it grows)
+	//file, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND, os.ModeAppend)
+
+	// for now, let's just create/overwrite the file each time
+	file, err := os.Create(path)
+	if err != nil {
+		if verboseMode {
+			// we don't want to return errors just because we cannot write logs to a file
+			// users can use the verbose flag to get full output to the console
+			fmt.Printf("Could not open log file %s for writing: %v\n", path, err)
 		}
 		return nil
 	}
@@ -69,7 +108,6 @@ func buildCliZapCoreConsoles(verboseMode bool) []zapcore.Core {
 	if !verboseMode {
 		consoleLoggerEncoderConfig.EncodeTime = nil
 		consoleLoggerEncoderConfig.LevelKey = ""
-		//consoleLoggerEncoderConfig.MessageKey = ""
 		consoleLoggerEncoderConfig.NameKey = ""
 	}
 	consoleEncoder := zapcore.NewConsoleEncoder(consoleLoggerEncoderConfig)
@@ -83,9 +121,9 @@ func buildCliZapCoreConsoles(verboseMode bool) []zapcore.Core {
 }
 
 // BuildCliLogger creates a logger that writes output to the specified filename
-func BuildCliLogger(filename, outputModeEnvVar string) *zap.SugaredLogger {
+func BuildCliLogger(pathElements []string, outputModeEnvVar string) *zap.SugaredLogger {
 	verboseMode := os.Getenv(outputModeEnvVar) == "1"
-	fileCore := buildCliZapCoreFile(filename, verboseMode)
+	fileCore := buildCliZapCoreFile(pathElements, verboseMode)
 	consoleCores := buildCliZapCoreConsoles(verboseMode)
 	allCores := consoleCores
 	if fileCore != nil {
@@ -94,22 +132,4 @@ func BuildCliLogger(filename, outputModeEnvVar string) *zap.SugaredLogger {
 	core := zapcore.NewTee(allCores...)
 	logger := zap.New(core).Sugar()
 	return logger
-}
-
-var outputModeEnvVar = "GLOOSHOT_CLI_OUTPUT_MODE"
-
-func getInitialContextAndSetLogger() context.Context {
-	contextutils.SetFallbackLogger(BuildCliLogger("~/.glooshot/log", outputModeEnvVar))
-	loggingContext := []interface{}{"version", version.Version}
-	ctx := contextutils.WithLogger(context.Background(), version.CliAppName)
-	ctx = contextutils.WithLoggerValues(ctx, loggingContext...)
-	return ctx
-}
-
-func main() {
-	ctx := getInitialContextAndSetLogger()
-	app := cli.App(ctx, version.Version)
-	if err := app.Execute(); err != nil {
-		contextutils.LoggerFrom(ctx).Fatalw("error during glooshot cli execution", zap.Error(err))
-	}
 }
