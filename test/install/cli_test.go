c3937163328986e4ff7a082090e8aba24a4223d4
Mitch Kelley <mitch.kelley@solo.io>
2019-04-17 21:11:14 -0400

    stack util, better cli output handling util

--- test/install/cli_test.go
+++ test/install/cli_test.go
@@ -21,55 +21,75 @@ var _ = Describe("Glooshot CLI", func() {
 | EXPERIMENT | NAMESPACE | STATUS |
 +------------+-----------+--------+
 +------------+-----------+--------+`
+	var standardCobraHelpBlockMatcher = MatchRegexp("Available Commands:")
 
 	BeforeEach(func() {
 		helpers.UseMemoryClients()
-		_, _ = glooshot("delete experiment --every-resource")
+		_, _, _ = glooshot("delete experiment --every-resource")
 	})
 
 	Context("basic args and flags", func() {
 		It("should return help messages without error", func() {
-			_, err := glooshot("-h")
+			_, _, err := glooshot("-h")
 			Expect(err).NotTo(HaveOccurred())
-			_, err = glooshot("help")
+			_, _, err = glooshot("help")
 			Expect(err).NotTo(HaveOccurred())
-			_, err = glooshot("--help")
+			_, _, err = glooshot("--help")
 			Expect(err).NotTo(HaveOccurred())
-		})
-
-		It("should return human-friendly errors on bad input", func() {
-			_, err := glooshot("--h")
-			Expect(err).To(HaveOccurred())
-			Expect(err.Error()).To(Equal("unknown flag: --h"))
 		})
 
 		It("should perform basic create, get, and delete commands", func() {
-			out, err := glooshot("get experiments --all-namespaces")
+			out, stdErr, err := glooshot("get experiments --all-namespaces")
 			Expect(err).NotTo(HaveOccurred())
+			Expect(stdErr).NotTo(HaveOccurred())
 			Expect(out).To(Equal(noResourcesTable))
 
-			out, err = glooshot("create experiment -f ../../examples/gs_delay.yaml")
+			out, stdErr, err = glooshot("create experiment -f ../../examples/gs_delay.yaml")
 			Expect(err).NotTo(HaveOccurred())
+			Expect(stdErr).NotTo(HaveOccurred())
 			Expect(out).To(Equal(""))
 
-			out, err = glooshot("get experiments --all-namespaces")
+			out, stdErr, err = glooshot("get experiments --all-namespaces")
 			Expect(err).NotTo(HaveOccurred())
+			Expect(stdErr).NotTo(HaveOccurred())
 			Expect(out).NotTo(Equal(noResourcesTable))
 
-			out, err = glooshot("delete experiments -n default --all")
+			out, stdErr, err = glooshot("delete experiments -n default --all")
 			Expect(err).NotTo(HaveOccurred())
+			Expect(stdErr).NotTo(HaveOccurred())
 			Expect(out).NotTo(Equal(noResourcesTable))
 
-			out, err = glooshot("get experiments --all-namespaces")
+			out, stdErr, err = glooshot("get experiments --all-namespaces")
 			Expect(err).NotTo(HaveOccurred())
+			Expect(stdErr).NotTo(HaveOccurred())
 			Expect(out).To(Equal(noResourcesTable))
 		})
 	})
+
+	Context("expect human-friendly errors", func() {
+
+		FIt("should return human-friendly errors on bad input", func() {
+			stdOut, stdErr, err := glooshot("--h")
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(Equal("unknown flag: --h"))
+			Expect(stdErr).NotTo(HaveOccurred())
+			Expect(stdOut).To(standardCobraHelpBlockMatcher)
+		})
+
+		It("should return human-friendly errors on bad input", func() {
+			stdOut, stdErr, err := glooshot("")
+			Expect(err).To(HaveOccurred())
+			Expect(stdErr).NotTo(HaveOccurred())
+			Expect(err.Error()).To(Equal("unknown flag: --h"))
+			Expect(stdOut).To(standardCobraHelpBlockMatcher)
+		})
+
+	})
 })
 
-func glooshot(args string) (string, error) {
+func glooshot(args string) (string, string, error) {
 	app := cli.App(context.Background(), "testglooshotcli")
-	return ExecuteCliOut(app, args)
+	return ExecuteCliOutErr(app, args)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -80,33 +100,62 @@ func ExecuteCli(command *cobra.Command, args string) error {
 	return command.Execute()
 }
 
-func ExecuteCliOut(command *cobra.Command, args string) (string, error) {
+func ExecuteCliOutErr(command *cobra.Command, args string) (string, string, error) {
 	stdOut := os.Stdout
 	stdErr := os.Stderr
-	r, w, err := os.Pipe()
+	r1, w1, err := os.Pipe()
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
-	os.Stdout = w
-	os.Stderr = w
+	r2, w2, err := os.Pipe()
+	if err != nil {
+		return "", "", err
+	}
+	os.Stdout = w1
+	os.Stderr = w2
 
 	command.SetArgs(strings.Split(args, " "))
 	err = command.Execute()
 
-	outC := make(chan string)
+	chan1 := make(chan string)
+	chan2 := make(chan string)
+
+	chan1err := make(chan error)
+	chan2err := make(chan error)
 
 	// copy the output in a separate goroutine so printing can't block indefinitely
 	go func() {
 		var buf bytes.Buffer
-		io.Copy(&buf, r)
-		outC <- buf.String()
+		_, err := io.Copy(&buf, r1)
+		chan1err <- err
+		chan1 <- buf.String()
+	}()
+	go func() {
+		var buf bytes.Buffer
+		_, err := io.Copy(&buf, r2)
+		chan2err <- err
+		chan2 <- buf.String()
 	}()
 
 	// back to normal state
-	w.Close()
 	os.Stdout = stdOut // restoring the real stdout
 	os.Stderr = stdErr
-	out := <-outC
+	if err := w1.Close(); err != nil {
+		return "", "", err
+	}
+	if err := w2.Close(); err != nil {
+		return "", "", err
+	}
+	if err := <-chan1err; err != nil {
+		return "", "", err
+	}
+	if err := <-chan2err; err != nil {
+		return "", "", err
+	}
+	capturedStdout := <-chan1
+	capturedStderr := <-chan2
 
-	return strings.TrimSuffix(out, "\n"), err
+	return strings.TrimSuffix(capturedStdout, "\n"),
+		strings.TrimSuffix(capturedStderr, "\n"),
+		err
 }
