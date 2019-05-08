5ac88b58fd033d2c601a9d3e56193831d9890fc8
Mitch Kelley <mitch.kelley@solo.io>
2019-05-08 15:20:52 -0400

    use utils from go-utils

--- test/install/install_suite_test.go
+++ test/install/install_suite_test.go
@@ -7,11 +7,11 @@ import (
 	"io/ioutil"
 	"os"
 	"regexp"
-	"runtime/debug"
 	"testing"
 
 	"github.com/avast/retry-go"
 	"github.com/solo-io/gloo-ee/pkg/log"
+	"github.com/solo-io/go-utils/testutils"
 	"github.com/solo-io/go-utils/testutils/clusterlock"
 	"github.com/solo-io/go-utils/testutils/kube"
 
@@ -36,7 +36,7 @@ func TestInstall(t *testing.T) {
 	}
 	helpers.RegisterPreFailHandler(
 		func() {
-			printTrimmedStack()
+			testutils.PrintTrimmedStack()
 		})
 	helpers.RegisterCommonFailHandlers()
 	helpers.SetupLog()
@@ -109,67 +109,4 @@ func printFileMatchers(file string, matchers []string) error {
 
 	}
 	return nil
-}
-
-// TODO(mitchdraft) - move to go-utils https://github.com/solo-io/go-utils/issues/131
-
-func printTrimmedStack() {
-	stack := debug.Stack()
-	fmt.Println(trimVendorStack(stack))
-}
-func trimVendorStack(stack []byte) string {
-	scanner := bufio.NewScanner(bytes.NewReader(stack))
-	ind := -1
-	pair := []string{}
-	skipCount := 0
-	output := ""
-	for scanner.Scan() {
-		ind++
-		if ind == 0 {
-			// skip the header
-			continue
-		}
-		pair = append(pair, scanner.Text())
-		if len(pair) == 2 {
-			evaluateStackPair(pair[0], pair[1], &output, &skipCount)
-			pair = []string{}
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
-	}
-	output = fmt.Sprintf("Stack trace (skipped %v entries that matched filter criteria):\n%v", skipCount, output)
-	return output
-}
-
-var (
-	funcRuntimeDebugRegex = &regexp.Regexp{}
-	fileVendorRegex       = &regexp.Regexp{}
-	fileSuiteRegex        = &regexp.Regexp{}
-	fileGoTestLibRegex    = &regexp.Regexp{}
-)
-
-func init() {
-	funcRuntimeDebugRegex = regexp.MustCompile("runtime/debug")
-	fileVendorRegex = regexp.MustCompile("vendor")
-	fileSuiteRegex = regexp.MustCompile("suite_test.go")
-	fileGoTestLibRegex = regexp.MustCompile("src/testing/testing.go")
-}
-
-func evaluateStackPair(functionLine, fileLine string, output *string, skipCount *int) {
-	skip := false
-	if funcRuntimeDebugRegex.MatchString(functionLine) {
-		skip = true
-	}
-	if fileVendorRegex.MatchString(fileLine) ||
-		fileSuiteRegex.MatchString(fileLine) ||
-		fileGoTestLibRegex.MatchString(fileLine) {
-		skip = true
-	}
-	if skip {
-		*skipCount = *skipCount + 1
-		return
-	}
-	*output = fmt.Sprintf("%v%v\n%v\n", *output, functionLine, fileLine)
-	return
 }
