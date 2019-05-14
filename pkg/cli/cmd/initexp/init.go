38ac8e0de1a9a00e407ed70403d725673f44569f
Mitch Kelley <mitch.kelley@solo.io>
2019-05-14 18:36:14 -0400

    cleanup unused code

--- pkg/cli/cmd/initexp/init.go
+++ pkg/cli/cmd/initexp/init.go
@@ -2,20 +2,15 @@ package initexp
 
 import (
 	"bytes"
-	"context"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"net/http"
 	"os"
 	"os/exec"
-	"strings"
+
+	"github.com/solo-io/glooshot/pkg/version"
 
 	"github.com/solo-io/glooshot/pkg/cli/flagutils"
-
-	"github.com/google/go-github/github"
-	"github.com/solo-io/glooshot/pkg/version"
-	"golang.org/x/oauth2"
 
 	"github.com/solo-io/glooshot/pkg/cli/options"
 	"github.com/spf13/cobra"
@@ -28,8 +23,7 @@ import (
 )
 
 const (
-	sgChartUriTemplate  = "https://storage.googleapis.com/glooshot-helm/charts/glooshot-%s.tgz"
-	tmpInstallNamespace = "glooshot"
+	gsChartUriTemplate = "https://storage.googleapis.com/glooshot-helm/charts/glooshot-%s.tgz"
 )
 
 func Cmd(opts *options.Options) *cobra.Command {
@@ -49,13 +43,10 @@ func Cmd(opts *options.Options) *cobra.Command {
 }
 
 func installGlooshot(opts *options.Options) error {
-	releaseVersion, err := getReleaseVersion(opts)
-	if err != nil {
-		return errors.Wrapf(err, "getting release version")
-	}
+	releaseVersion := version.Version
 
 	// Get location of Gloo helm chart
-	chartUri := fmt.Sprintf(sgChartUriTemplate, releaseVersion)
+	chartUri := fmt.Sprintf(gsChartUriTemplate, releaseVersion)
 	if helmChartOverride := opts.Init.HelmChartOverride; helmChartOverride != "" {
 		chartUri = helmChartOverride
 	}
@@ -66,7 +57,7 @@ func installGlooshot(opts *options.Options) error {
 	}
 
 	if _, err := opts.Clients.KubeClient().CoreV1().Namespaces().Create(&v1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{Name: tmpInstallNamespace},
+		ObjectMeta: metav1.ObjectMeta{Name: opts.Init.InstallNamespace},
 	}); err != nil && !kubeerrs.IsAlreadyExists(err) {
 		return errors.Wrapf(err, "creating namespace")
 	}
@@ -75,11 +66,11 @@ func installGlooshot(opts *options.Options) error {
 		chartUri,
 		values,
 		"glooshot",
-		tmpInstallNamespace,
+		opts.Init.InstallNamespace,
 		"",
 	)
 	if err != nil {
-		return errors.Wrapf(err, "rendering manifests")
+		return errors.Wrapf(err, "rendering manifest from uri: %v", chartUri)
 	}
 
 	manifest := manifests.CombinedString()
@@ -91,30 +82,12 @@ func installGlooshot(opts *options.Options) error {
 
 	fmt.Printf("installing glooshot version %v\nusing chart uri %v\n", releaseVersion, chartUri)
 
-	if err := kubectlApply(manifest, tmpInstallNamespace); err != nil {
+	if err := kubectlApply(manifest, opts.Init.InstallNamespace); err != nil {
 		return errors.Wrapf(err, "executing kubectl failed")
 	}
 
 	fmt.Printf("install successful!\n")
 	return nil
-}
-
-func getReleaseVersion(opts *options.Options) (string, error) {
-	//if !version.IsReleaseVersion() {
-	//	if opts.Init.ReleaseVersion == "" {
-	//		return "", errors.Errorf("you must provide a " +
-	//			"release version containing the manifest when " +
-	//			"running an unreleased version of glooshot.")
-	//	} else if opts.Init.ReleaseVersion == "latest" {
-	//		releaseVersion, err := helpers.GetLatestVersion(opts.Ctx, "glooshot")
-	//		if err != nil {
-	//			return "", errors.Wrapf(err, "unable to retrieve latest release version from github")
-	//		}
-	//		return releaseVersion, nil
-	//	}
-	//	return opts.Init.ReleaseVersion, nil
-	//}
-	return version.Version, nil
 }
 
 func readValues(path string) (string, error) {
@@ -140,34 +113,4 @@ func kubectl(stdin io.Reader, args ...string) error {
 	kubectl.Stdout = os.Stdout
 	kubectl.Stderr = os.Stderr
 	return kubectl.Run()
-}
-
-func GetLatestVersion(ctx context.Context, repo string) (string, error) {
-	client := GetPublicGithubClient(ctx)
-	release, _, err := client.Repositories.GetLatestRelease(ctx, "solo-io", repo)
-	if err != nil {
-		return "", errors.Wrapf(err, "unable to get latest version for %s", repo)
-	}
-	return release.GetTagName()[1:], nil
-}
-
-func IsValidVersion(ctx context.Context, repo, version string) (string, error) {
-	version = "v" + strings.TrimPrefix(version, "v")
-	client := GetPublicGithubClient(ctx)
-	release, _, err := client.Repositories.GetReleaseByTag(ctx, "solo-io", repo, version)
-	if err != nil {
-		return "", errors.Wrapf(err, "%s is not a valid version for %s", version, repo)
-	}
-	return release.GetTagName()[1:], nil
-}
-
-func GetPublicGithubClient(ctx context.Context) *github.Client {
-	client := http.DefaultClient
-	if githubToken := os.Getenv("GITHUB_TOKEN"); githubToken != "" {
-		ts := oauth2.StaticTokenSource(
-			&oauth2.Token{AccessToken: githubToken},
-		)
-		client = oauth2.NewClient(ctx, ts)
-	}
-	return github.NewClient(client)
 }
--- pkg/cli/flagutils/metadata.go
+++ pkg/cli/flagutils/metadata.go
@@ -13,12 +13,16 @@ func AddMetadataFlags(set *pflag.FlagSet, in *core.Metadata) {
 }
 
 func AddInitFlags(set *pflag.FlagSet, init *options.Init) {
-	set.StringVarP(&init.HelmChartOverride, "file", "f", "", "Install Glooshot from this Helm chart rather than from a release. Target file must be a tarball")
-	set.StringVarP(&init.HelmValues, "values", "v", "", "Provide a custom values.yaml overrides for the installed helm chart. Leave empty to use default values from the chart.")
-	set.StringVarP(&init.InstallNamespace, "namespace", "n", "glooshot", "namespace to install glooshot into")
+	set.StringVarP(&init.HelmChartOverride, "file", "f", "",
+		"Install Glooshot from this Helm chart rather than from a release. Target file must be a tarball.")
+	set.StringVarP(&init.HelmValues, "values", "v", "",
+		"Provide a custom values.yaml overrides for the installed helm chart. Leave empty to use default values from the chart.")
+	set.StringVarP(&init.InstallNamespace, "namespace", "n", "glooshot",
+		"namespace to install glooshot into")
 	if !version.IsReleaseVersion() {
 		set.StringVar(&init.ReleaseVersion, "release", "", "install from this release version. Should correspond with the "+
 			"name of the release on GitHub")
 	}
-	set.BoolVarP(&init.DryRun, "dry-run", "d", false, "Dump the raw installation yaml instead of applying it to kubernetes")
+	set.BoolVarP(&init.DryRun, "dry-run", "d", false,
+		"Dump the raw installation yaml instead of applying it to kubernetes")
 }
