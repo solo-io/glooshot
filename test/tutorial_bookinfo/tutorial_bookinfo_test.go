cede5d5526dd8e91bbdf1f8af38c3c08341b3c00
Mitch Kelley <mitch.kelley@solo.io>
2019-05-20 17:54:10 +0200

    add test to ci

--- test/tutorial_bookinfo/tutorial_bookinfo_test.go
+++ test/tutorial_bookinfo/tutorial_bookinfo_test.go
@@ -1,20 +1,13 @@
 package tutorial_bookinfo
 
 import (
-	"fmt"
-	"os"
-
 	. "github.com/onsi/ginkgo"
 )
 
 var _ = Describe("Glooshot", func() {
 
 	It("should complete the bookinfo tutorial successfully", func() {
-		// TODO(mitchdraft) - restore when prometheus is deployed to the test env.
-		if os.Getenv("CI_TESTS") == "1" {
-			fmt.Printf("this test is disabled in CI. to run, ensure env var `CI_TESTS` is not set to 1")
-			return
-		}
+		// if setup runs correctly, the test has passed
 	})
 
 })
